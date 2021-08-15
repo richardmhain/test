097e88a57dde1ccb07cfa1e6125455db4c815801
richardmhain <31418888+richardmhain@users.noreply.github.com>
2021-08-14 21:12:14 -0400

    1st DOS Script
    Testing out sample codes...

---
+++ dos.cmd
@@ -0,0 +1,7 @@
+echo "# test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/richardmhain/test.git
+git push -u origin master
